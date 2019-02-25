8c1a2be892e93684c24efcc9710310499a5dc46e
pablobovi <pablobovi18@gmail.com>
2019-02-25 16:06:56 -0300

    agregar funciones por nivel

--- sis_menu.php
+++ sis_menu.php
@@ -1,6 +1,5 @@
 <div class="sidebar-nav">
 <ul>
-<?php if($_SESSION["tipo_usuario"]!=2){ ?>
 <li><a href="principal.php" class="nav-header"><i class="fa fa-home"></i> Home</a></li>
 <li><a href="#" data-target=".dashboard-menu" class="nav-header collapsed" data-toggle="collapse"><i class="fa fa-shopping-cart"></i> Ventas<i class="fa fa-collapse"></i></a></li>
 <li>
@@ -12,6 +11,7 @@
 <!--
 <li data-popover="true" data-content="Acciones de las estaciones." rel="popover" data-placement="right">
 -->
+<?php if($_SESSION["nivel"]>=2){ ?>
 <li><a href="#" data-target=".premium-menu" class="nav-header collapsed" data-toggle="collapse"><i class="fa fa-shopping-basket"></i> Compras<i class="fa fa-collapse"></i></a></li>
 <li>
   <ul class=" premium-menu nav nav-list collapse">
@@ -20,6 +20,8 @@
     <li><a href="historico_compras.php"><span class="fa fa-caret-right"></span> Listado de compras </a></li>
   </ul>
 </li>
+<?php } ?>
+<?php if($_SESSION["nivel"]==5){ ?>
 <li><a href="#" data-target=".legal-menu" class="nav-header collapsed" data-toggle="collapse"><i class="fa fa-edit"></i> Liquidaci&oacute;n de sueldos<i class="fa fa-collapse"></i></a></li>
 <li>
   <ul class="legal-menu nav nav-list collapse">
@@ -29,7 +31,6 @@
     <li><a href="conceptos.php"><span class="fa fa-caret-right"></span> Conceptos</a></li>
   </ul>
 </li>
-
 <li><a href="#" data-target=".item2-menu" class="nav-header collapsed" data-toggle="collapse"><i class="fa fa-users"></i> Empleados<i class="fa fa-collapse"></i></a></li>
 <li>
   <ul class="item2-menu nav nav-list collapse">
@@ -37,6 +38,8 @@
     <li><a href="categoriaempleado.php"><span class="fa fa-caret-right"></span> Categorias</a></li>
   </ul>
 </li>
+<?php } ?>
+<?php if($_SESSION["nivel"]>=1){ ?>
 <li><a href="#" data-target=".item-menu" class="nav-header collapsed" data-toggle="collapse"><i class="fa fa-barcode"></i> Productos<i class="fa fa-collapse"></i></a></li>
 <li>
   <ul class="item-menu nav nav-list collapse">
@@ -44,6 +47,8 @@
     <li><a href="categoriaproducto.php"><span class="fa fa-caret-right"></span> Categorias</a></li>
   </ul>
 </li>
+<?php } ?>
+<?php if($_SESSION["nivel"]>=4){ ?>
 <li><a href="#" data-target=".item3-menu" class="nav-header collapsed" data-toggle="collapse"><i class="fa fa-book"></i> Reportes<i class="fa fa-collapse"></i></a></li>
 <li>
   <ul class="item3-menu nav nav-list collapse">
@@ -56,12 +61,5 @@
 <li><a href="proveedores.php" class="nav-header"><i class="fa fa-truck"></i> Proveedores</a></li>
 <li><a href="clientes.php" class="nav-header"><i class="fa fa-male"></i> Clientes</a></li>
 <li><a href="pago.php" class="nav-header"><i class="fa fa-money"></i> Cuenta Clientes</a></li>
-<?php } else { ?>
-<li><a href="#" data-target=".dashboard-menu" class="nav-header" data-toggle="collapse"><i class="fa fa-shopping-cart"></i> Ventas<i class="fa fa-collapse"></i></a></li>
-<li>
-  <ul class="dashboard-menu nav nav-list collapse in">
-    <li><a href="venta_nueva.php"><span class="fa fa-caret-right"></span> Crear Venta</a></li>
-  </ul>
-</li>
 <?php } ?>
  </div>
