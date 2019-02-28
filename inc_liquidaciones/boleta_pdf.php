e5bd5ba16fa1414a79e5ecb7d024c648b3def09f
Navarro Bovi <pablo.navarro@sovos.com>
2019-02-28 15:03:07 -0300

    Update boleta_pdf.php

--- inc_liquidaciones/boleta_pdf.php
+++ inc_liquidaciones/boleta_pdf.php
@@ -60,54 +60,24 @@ function Footer()
 $db = new dbObj();
 $connString =  $db->getConnstring();
 
-/* 
-// $q_compra=mysql_query("SELECT * FROM compra");
-mysql_select_db($database_conexion_smile,$conexion_smile);
-$result = mysqli_query($connString, "SELECT descripcionconcepto, cantidad, totalhaber, tipoconcepto FROM detalleliquidacion 
-INNER JOIN detalleconcepto ON detalleliquidacion_iddetalleliquidacion=iddetalleliquidacion
-INNER JOIN concepto ON concepto_idconcepto=idconcepto WHERE empleado_idempleado=$idempleado AND tipoconcepto='0'") or die("database error:". mysqli_error($connString));
-
-$total1 = 0;
-while($row=mysqli_fetch_assoc($result))
-  {
-    $total1 = $total1 + $row['totalhaber'];
-  }
-
-$pdf = new PDF();
-//header
-$pdf->AddPage('L','A4',0);
-//foter page
-$pdf->AliasNbPages();
-$pdf->SetTextColor(39, 138, 226);
-$pdf->SetFont('Arial','B',9);
-$pdf->Cell(31,8,"Concepto",1,0,'C');
-$pdf->Cell(31,8,"Cantidad",1,0,'C');
-$pdf->Cell(31,8,"Haber",1,0,'C');
-$pdf->Cell(31,8,"Tipo concepto",1,0,'C');
-
-foreach($result as $row) {
-  $pdf->SetTextColor(100);
-  $pdf->SetFont('Arial','',9);
-  $pdf->Ln();
-  foreach($row as $column)
-  $pdf->Cell(31,8,$column,1,0,'C');
-}
-*/
 mysql_select_db($database_conexion_smile,$conexion_smile);
 $result = mysqli_query($connString, "SELECT descripcionconcepto, cantidad, subtotal,  NULL 
 FROM detalleliquidacion 
 INNER JOIN detalleconcepto ON detalleliquidacion_iddetalleliquidacion=iddetalleliquidacion
 INNER JOIN concepto ON concepto_idconcepto=idconcepto 
-WHERE empleado_idempleado=7 AND tipoconcepto='1' 
-AND iddetalleliquidacion IN (SELECT MAX(iddetalleliquidacion) FROM detalleliquidacion)
+WHERE empleado_idempleado=$idempleado AND tipoconcepto='1' 
+AND iddetalleliquidacion IN (SELECT MAX(iddetalleliquidacion) FROM detalleliquidacion
+                            where  empleado_idempleado = $idempleado                            
+                            )
 UNION
 
 SELECT descripcionconcepto, cantidad,  NULL, subtotal 
 FROM detalleliquidacion 
 INNER JOIN detalleconcepto ON detalleliquidacion_iddetalleliquidacion=iddetalleliquidacion
 INNER JOIN concepto ON concepto_idconcepto=idconcepto 
-WHERE empleado_idempleado=7 AND tipoconcepto='0'
-AND iddetalleliquidacion IN (SELECT MAX(iddetalleliquidacion) FROM detalleliquidacion)") or die("database error:". mysqli_error($connString));
+WHERE empleado_idempleado=$idempleado AND tipoconcepto='0'
+AND iddetalleliquidacion IN (SELECT MAX(iddetalleliquidacion) FROM detalleliquidacion
+                              where  empleado_idempleado = $idempleado  )") or die("database error:". mysqli_error($connString));
 
 $pagototal =  mysqli_query($connString, "SELECT pagototal 
 FROM detalleliquidacion 
@@ -118,7 +88,7 @@ or die("database error:". mysqli_error($connString));
 $total = 0;
 while($row=mysqli_fetch_assoc($pagototal))
   {
-    $total = $total + $row['pagototal'];
+    $total = $total + ($row['pagototal']* -1);
   }
 
 $pdf = new PDF();
